fd0c98b61643778014657e879065368e3b667b2d
a25814740 <a25814740@gmail.com>
2022-05-25 21:30:11 +0800

    header, 數字js先註解

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@ const App = {
             this.myFullpage();
             this.bannerSwiper();
             this.situationChart();
-            this.situationNumEffect();
+            // this.situationNumEffect();
                 // this.introScene();
                 // this.introWheel();
             this.vaccineScene();
